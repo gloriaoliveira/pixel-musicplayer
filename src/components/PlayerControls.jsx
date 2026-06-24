a4c301efeaae70a303158da9a183580f2e84c1ff
Gloria Oliveira <gloriaost@gmail.com>
2026-06-23 23:49:56 -0300

    criacao de esqueleto

--- src/components/PlayerControls.jsx
+++ src/components/PlayerControls.jsx
@@ -2,7 +2,9 @@ function PlayerControls() {
   return (
     <div className="player-controls">
         <div className="album-cover"></div>
+        <span className="now-playing">Now playing...</span>
         <div className="song-title">Song Title</div>
+        <div className="artist-name">Artist Name</div>
         <div className="player-btns">
             <button>⏮️</button>   {/* anterior */}
             <button>⏯️</button>   {/* play/pause */}
